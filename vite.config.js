75ee9b3d274dc6e3a6e839bf5db2188b261161d9
nathandekeyrel <natedekeyrel@gmail.com>
2024-11-30 14:18:24 -0700

    config stuff

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    assetsInclude: ['**/*.glb']
+    assetsInclude: ['**/*.glb'],
+    resolve: {
+        alias: {
+            'src': '/src'
+        }
+    },
+    build: {
+        rollupOptions: {
+            external: [
+                // Add external dependencies here if needed
+            ]
+        }
+    }
 })
